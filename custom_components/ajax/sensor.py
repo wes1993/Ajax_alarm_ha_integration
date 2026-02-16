3c14f35562662e115cb8896dc7d19a0f98a7dd33
wes1993 <52070589+wes1993@users.noreply.github.com>
2026-02-16 19:50:43 +0100

    Update sensor.py

--- custom_components/ajax/sensor.py
+++ custom_components/ajax/sensor.py
@@ -10,7 +10,7 @@ async def async_setup_entry(hass, entry, async_add_entities):
     entities = []
     data = hass.data[DOMAIN][entry.entry_id]
     api = hass.data[DOMAIN][entry.entry_id]["api"]
-    #_LOGGER.error("SETUP ENTRY: %s", devices_by_hub)
+    _LOGGER.error("SETUP ENTRY: %s", devices_by_hub)
     for hub_id, devices in devices_by_hub.items():
 
         for device in devices:
