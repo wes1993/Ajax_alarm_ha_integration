6ae571b7215672d640def6766a682e29f4c4fc0d
wes1993 <52070589+wes1993@users.noreply.github.com>
2026-02-16 18:30:33 +0100

    Update sensor.py

--- custom_components/ajax/sensor.py
+++ custom_components/ajax/sensor.py
@@ -10,8 +10,9 @@ async def async_setup_entry(hass, entry, async_add_entities):
     entities = []
     data = hass.data[DOMAIN][entry.entry_id]
     api = hass.data[DOMAIN][entry.entry_id]["api"]
-
+    _LOGGER.error("SETUP ENTRY: %s", devices_by_hub)
     for hub_id, devices in devices_by_hub.items():
+
         for device in devices:
             for platform, meta in map_ajax_device(device):
                 if platform != "sensor":
@@ -25,7 +26,7 @@ async def async_setup_entry(hass, entry, async_add_entities):
                 else:
                     entity = AjaxSensor(device, meta, hub_id, api)
                 entities.append(entity)
-
+    _LOGGER.error("ENTITY: %s", entities)
     async_add_entities(entities)
 
 
